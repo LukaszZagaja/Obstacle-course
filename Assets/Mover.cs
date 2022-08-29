1a8aca5f5e4d2ff46bc3ef88ab51e5bb16464258
Luuq <lukasz2zagaja@gmail.com>
2022-08-29 22:10:31 +0200

    Basics
    added several variables with serialized fields and materials to player and ground

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
