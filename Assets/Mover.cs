a3451ee31bf01a4db1f584e329347f6680e82459
Luuq <lukasz2zagaja@gmail.com>
2022-08-30 16:25:24 +0200

    Changed movement
    changed movement from "free" to wsad

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0f;
-
     void Start()
     {
         
@@ -15,6 +11,8 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0, zValue);
     }
 }
