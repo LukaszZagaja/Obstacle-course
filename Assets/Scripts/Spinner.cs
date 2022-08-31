b3b7f8df5798069498288c5884f445b23ec226c8
Luuq <lukasz2zagaja@gmail.com>
2022-08-31 15:59:36 +0200

    Spinning
    added spinning obstacle

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -1,18 +1,14 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float xAngle = 0;
+    [SerializeField] float yAngle = 0;
+    [SerializeField] float zAngle = 0;
     void Update()
     {
-        
+        transform.Rotate (xAngle, yAngle, zAngle);
     }
 }
